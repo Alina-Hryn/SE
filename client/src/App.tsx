20173284f60ccfdbde07540d3bcb175ff91527e3
Rostyslav Makhanko <rostyslavm@wix.com>
2020-12-08 13:15:46 +0200

    Add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Button } from "@material-ui/core";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header tabValue={0} />
       </header>
+
       <body>
         <Button variant="contained" color="primary">
           Click
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,31 @@
-export const Header = () => {
+import * as React from "react";
+import { AppBar } from "@material-ui/core";
+import Tab from "@material-ui/core/Tab";
+import { TabPanel } from "@material-ui/lab";
+import Tabs from "@material-ui/core/Tabs";
+import { useState } from "react";
+
+type Props = {
+  tabValue: number;
+};
+
+export const Header = (props: Props) => {
+  const [tabValue, setTabValue] = useState(props.tabValue);
+
   return (
     <div>
-      <a>Cinemas</a>
+      <AppBar position="static">
+        <Tabs
+          value={tabValue}
+          onChange={(event, value) => setTabValue(value)}
+          aria-label="simple tabs example"
+        >
+          <Tab label="AVARO" />
+          <Tab label="SEANCES" />
+          <Tab label="MOVIES" />
+          <Tab label="CINEMAS" />
+        </Tabs>
+      </AppBar>
     </div>
   );
 };
